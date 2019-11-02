ac89aa6a7f68eb54aad90ba100c80d791e825de6
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2019-11-02 16:11:29 +0100

    Fix hydra script

--- hydra/index.js
+++ hydra/index.js
@@ -4,8 +4,8 @@ const loop = require('raf-loop')
 function init () {
   const canvas = document.createElement('canvas')
   canvas.style.backgroundColor = "#000"
-  canvas.width = 800
-  canvas.height = 200
+  canvas.width = 1024
+  canvas.height = 768
   canvas.style.width = '100%'
   canvas.style.height = '100%'
 
@@ -20,32 +20,18 @@ function init () {
   window.hydra = hydra
 
   a.show()
-  a.setScale(10)
+  a.setScale(1)
   a.setBins(6)
-  a.settings[0].cutoff = 1
-  a.settings[1].cutoff = 2
-  a.settings[2].cutoff = 4
-  a.settings[3].cutoff = 6
-  a.settings[4].cutoff = 8
-  a.settings[5].cutoff = 9
-
-  // osc(322).color(0,0,0)
-  // .add(shape(2).color(2,2,2).scale(0.006).rotate(0.000001))
-  // .scale(1.2,1,3)
-  // //.scale(()=> a.fft[3]*0.1 -2)
-  // //.repeat(1,5)
-  // //.rotate(1,()=> a.fft[3]*1 +0.01)
-  // .out(o0)
 
   voronoi(4, 1.8)
-    .modulate(noise(()=> a.fft[2]*10 +0.01).scale(5,0.1))
+    .modulate(noise(()=> a.fft[1]*110 + 0.01).scale(5,0.1))
     .rotate(1.57, 0.0)
     .thresh(0.3)
     .out(o1)
 
   src(o1)
     .mult(
-      osc(10)
+      osc(1, 0.12)
       .rotate(1.58))
     .out(o2)
 
@@ -54,10 +40,10 @@ function init () {
     .out(o3)
 
   src(o3)
-    .scale(1.2)
+    .scale(2.2)
     .modulateRotate(
-      osc(200, -0.02)
-      .rotate(0.5), -0.3)
+      osc(2000, -0.02)
+      .rotate(0.8), -0.3)
       .out()
 
 // render()
