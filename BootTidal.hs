e12b5482f1811c9d1dce64ac589a40b85dfd35b5
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2020-01-02 09:24:56 +0100

    Raise latency to make it work with crappy soundcards

--- BootTidal.hs
+++ BootTidal.hs
@@ -7,7 +7,7 @@ import System.IO (hSetEncoding, stdout, utf8)
 hSetEncoding stdout utf8
 
 -- total latency = oLatency + cFrameTimespan
-tidal <- startTidal (superdirtTarget {oLatency = 0.1, oAddress = "127.0.0.1", oPort = 57120}) (defaultConfig {cFrameTimespan = 1/20})
+tidal <- startTidal (superdirtTarget {oLatency = 0.3, oAddress = "127.0.0.1", oPort = 57120}) (defaultConfig {cFrameTimespan = 1/20})
 
 :{
 let p = streamReplace tidal
