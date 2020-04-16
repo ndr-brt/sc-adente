c450de9b945d8c56e85f2033443ea4451b0c4b4a
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2020-04-16 23:38:09 +0200

    Refactor

--- p5js/sinetane.js
+++ p5js/sinetane.js
@@ -2,7 +2,6 @@
 
 export default function(p) {
 
-  let centerX = p.windowWidth/2
   let centerY = p.windowHeight/2
 
   p.setup = () => {
@@ -18,15 +17,25 @@ export default function(p) {
     let a = 0.0;
     let factor = 12 + p.sin(p.frameCount/200)*5
     let inc = p.TWO_PI / factor;
+    let factorUp = factor * 2 * p.tan(p.sqrt(p.frameCount/16)) / (p.cos(p.sqrt(p.frameCount/25) % 5) + 3)
+    let factorDown = factor * factor / (1 + (p.tan(p.sqrt(a) * ((p.frameCount/p.pow(factor,3)) % factor)+factor)))
     for (let i = 0; i < p.windowWidth/factor; i++) {
       let sine = p.sin((i * p.frameCount)/(p.pow(factor, 2)))
+
       p.line(
         i * factor,
-        centerY - (sine*factor*2 * p.tan(p.sqrt(p.frameCount/16)) / (p.cos(p.sqrt(p.frameCount/25) % 5) + 3)),
-        i * factor,
-        centerY + (sine*factor*factor / (1 + (p.tan(p.sqrt(a) * ((p.frameCount/p.pow(factor,3)) % p.pow(factor,2))+factor))))
+        centerY - (sine * factorUp),
+        a + i * factor,
+        centerY + (sine * factorDown)
       );
-      a = a + inc;
+
+      let spostamento = inc * p.sin(p.sqrt(p.frameCount));
+      if (a > p.frameCount) {
+        a += spostamento;
+      } else {
+        a -= spostamento;
+      }
+
     }
   }
 
