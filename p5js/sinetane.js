db8c8727b13214b7560ceb01cbca528e81acc299
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2020-04-16 23:01:23 +0200

    Improve sketch

--- p5js/sinetane.js
+++ p5js/sinetane.js
@@ -12,19 +12,19 @@ export default function(p) {
   p.draw = () => {
     p.background(20);
 
-    p.stroke(255);
+    p.stroke(125);
     p.strokeWeight(3);
 
     let a = 0.0;
-    let inc = p.TWO_PI / 25.0;
-    let factor = 24
+    let factor = 12 + p.sin(p.frameCount/200)*5
+    let inc = p.TWO_PI / factor;
     for (let i = 0; i < p.windowWidth/factor; i++) {
       let sine = p.sin((i * p.frameCount)/(p.pow(factor, 2)))
       p.line(
         i * factor,
-        centerY + sine*factor*(2) * p.tan(p.sqrt(p.frameCount)),
+        centerY - (sine*factor*2 * p.tan(p.sqrt(p.frameCount/16)) / (p.cos(p.sqrt(p.frameCount/25) % 5) + 3)),
         i * factor,
-        centerY - sine*factor*factor / (1 + (p.tan(p.sqrt(p.frameCount) * ((p.frameCount/p.pow(factor,3)) % p.pow(factor,2))+factor)))
+        centerY + (sine*factor*factor / (1 + (p.tan(p.sqrt(a) * ((p.frameCount/p.pow(factor,3)) % p.pow(factor,2))+factor))))
       );
       a = a + inc;
     }
