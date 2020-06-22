e2635f8582fcdb9a0b9f755a0477c209ee1117b3
ndr_brt <andrea.bertagnolli@cherrychain.it>
2020-06-22 09:19:31 +0200

    Bau

--- p5js/cosin.js
+++ p5js/cosin.js
@@ -8,8 +8,9 @@ export default function(p) {
   let centerY = height/2;
   let divisorX = 13;
   let divisorY = 11;
-  let maxPoints = 1200;
-  let strokeWeight = 10;
+  let maxPoints = 1000;
+  let strokeWeight = 9;
+  let stroke = 50;
   let points = []
 
   p.setup = () => {
@@ -18,13 +19,9 @@ export default function(p) {
 
   p.draw = () => {
     p.background(10);
-
-    p.stroke(125);
     p.noFill();
 
-    // let shiftX = p.sin(p.frameCount/17)*(centerX*p.cos(p.frameCount))
     let shiftX = p.sin(p.frameCount/divisorX)* centerX
-    // let shiftY = p.sin(p.frameCount/8)*(centerY*p.sin(p.frameCount))
     let shiftY = p.cos(p.frameCount/divisorY) * centerY
 
     points.push(p.createVector(centerX + shiftX, centerY + shiftY))
@@ -32,13 +29,14 @@ export default function(p) {
     for (var i = 1; i < points.length; i++) {
       let prec = points[i-1]
       let actual = points[i]
+      p.stroke(stroke*(p.sin(p.frameCount/i)+1))
       p.strokeWeight(strokeWeight*(i/points.length));
       p.line(prec.x, prec.y, actual.x, actual.y)
     }
 
     if (p.frameCount % maxPoints == 0) {
-      divisorX = p.random(15) + 1
-      divisorY = p.random(15) + 1
+      divisorX = p.random(10) + 5
+      divisorY = p.random(10) + 5
     }
 
     if (points.length > maxPoints) {
