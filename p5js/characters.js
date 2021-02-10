be0779d5bcf43bb3259ba8d60749b138392f42e0
ndr_brt <andrea.bertagnolli@cherrychain.it>
2021-02-10 23:35:58 +0100

    Improve sine visuals

--- p5js/characters.js
+++ p5js/characters.js
@@ -15,7 +15,6 @@ export default function(p) {
   p.draw = (() => {
     p.textFont('Helvetica');
     p.background(0);
-    p.textSize(50);
     for (var i = 0; i < characters.length; i++) {
       var character = characters[i]
       p.textSize(character.size)
@@ -38,25 +37,32 @@ export default function(p) {
     for (var i = 0; i < oscMessage.args.length; i+=2) {
       message[oscMessage.args[i]] = oscMessage.args[i+1]
     }
-    console.log(message)
+    // console.log(message)
 
     // oc=1 n=0: center (centerX, centerY)
+    let cycle = message['cycle']
     let octave = message['octave']
     let note = message['n']
     let delta = message['delta']
     let legato = message['legato']
     let speed = message['speed']
-    let pan = message['pan'] - 1
+    let pan = message['pan'] - 0.5
+
+    let cycleNumber = p.floor(cycle)
+    let cycleOffset = cycle - cycleNumber
 
     let distanceFromCenter = centerY / noteSpectre * (octave*12 + note);
-    let angle = p.random(360)
-    // console.log(`angle: ${angle}. sin: ${p.sin(angle)}. cos: ${p.cos(angle)}`)
-    let cathetusX = distanceFromCenter * p.cos(angle) + (pan*distanceFromCenter)
+    let quadrant = cycleNumber + octave
+
+    let offset = cycleNumber % 2 == 0 ? (1 - cycleOffset) : cycleOffset
+    let angle = p.PI/1.5 * (offset + quadrant)
+
+    let cathetusX = distanceFromCenter * p.cos(angle) + (pan * centerX)
     let cathetusY = distanceFromCenter * p.sin(angle)
 
     characters.push({
       symbol: String.fromCharCode(delta*100*p.random(2) + 30),
-      size: 40 * (legato/4+0.75),
+      size: 50 * (legato/4+0.75),
       speed: speed,
       alpha: 255,
       x: centerX + cathetusX,
