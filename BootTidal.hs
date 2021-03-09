a9f1acf08cdab438fba49899426c054be73de27a
ndr_brt <andrea.bertagnolli@cherrychain.it>
2021-03-09 17:38:39 +0100

    Sine stuff

--- BootTidal.hs
+++ BootTidal.hs
@@ -7,8 +7,9 @@ hSetEncoding stdout utf8
 
 :{
 let oscmap = [
-              ((superdirtTarget {oLatency = 0.15, oPort = 57120}), [superdirtShape]),
-              ((superdirtTarget {oLatency = 0.15, oPort = 57121}), [superdirtShape])
+              ((superdirtTarget {oLatency = 0.15, oPort = 57120}), [superdirtShape])
+              -- ,
+              -- ((superdirtTarget {oLatency = 0.15, oPort = 57121}), [superdirtShape])
              ]
 :}
 
