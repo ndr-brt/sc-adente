8faa01bfbad2d03dfdd1f35f33c3bb979224fae5
ndr_brt <andrea.bertagnolli@gmail.com>
2022-08-05 14:27:02 +0200

    live

--- hydra/elea_9003.js
+++ hydra/elea_9003.js
@@ -8,15 +8,15 @@ vid.src = '/home/andrea/Videos/elea_9003.mp4'
 s0.init({ src: vid })
 
 a.show()
-a.setSmooth(0.6)
+a.setSmooth(0.8)
 a.setBins(4);
 
 src(s0)
   .kaleid(({time}) => (Math.abs(Math.sin(time/10))+1)*4)
   .scale(({time}) => Math.sin(time/2000))
   .rotate(({time}) => Math.cos(time/10))
-  .thresh(({time}) => 0.8 - a.fft[0]/2)
-  .contrast(({time}) => a.fft[1]+1)
+  .thresh(({time}) => 0.8 - a.fft[1]/3)
+  .contrast(({time}) => a.fft[2]+1)
   // .add(o0)
   .blend(src(o0).rotate(({time}) => Math.sin(time/20)))
   // .blend(src(o0).rotate(4))
