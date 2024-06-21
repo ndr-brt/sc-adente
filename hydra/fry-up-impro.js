1e727e79bbcec90dbe0b39124d6cd9e3f18de0cd
ndr_brt <andrea.bertagnolli@gmail.com>
2024-06-21 15:58:37 +0200

    updates

--- hydra/fry-up-impro.js
+++ hydra/fry-up-impro.js
@@ -2,22 +2,26 @@ a.show()
 a.setSmooth(0.2)
 a.setBins(4);
 
-noise(() => 6 * a.fft[1],.05)
+// noise(() => 6 * a.fft[1],.05)
+src(o1)
 .mult( osc(9,0, ()=>Math.sin(time/1.5)+2 ) )
 .mult(
-    noise(9,.03).brightness(1.2).contrast(2)
+    noise(9,.03).brightness(3.2).contrast(2)
     .mult( osc(9,0, ()=>Math.sin(time/3)+13 ) )
 )
-.diff(
-    noise(15,.04).brightness(.2).contrast(1.3)
-    .mult( osc(9,0, ()=>Math.sin(time/5)+13 ) )
-    .rotate( ()=>time/33 )
-)
-.scale( ()=>Math.sin(time/6.2)*.12+.15 )
-.modulateScale(
-    osc(16,() => a.fft[2],0).mult( osc(3,0,0).rotate(3.14/2) )
-    .rotate( ()=>time/25 ).scale(.39).scale(1,.6,1).invert()
-    , ()=>Math.sin(time/5.3)*1.5+3  )
-.rotate( ()=>time/22 )
-.mult( shape(100,() => a.fft[0] + 0.1,.01).scale(1,.6,1) )
+// .diff(
+//     noise(15,.04).brightness(.2).contrast(1.3)
+//     .mult( osc(9,0, ()=>Math.sin(time/5)+13 ) )
+//     .rotate( ()=>time )
+// )
+// .scale( ()=>Math.sin(time/6.2)*.12+.15 )
+// .modulateScale(
+//     osc(16,() => a.fft[2],0).mult( osc(3,0,0).rotate(3.14/2) )
+//     .rotate( ()=>time/25 ).scale(.39).scale(1,.6,1).invert()
+//     , ()=>Math.sin(time/5.3)*1.5+3  )
+// .rotate( ()=>time/22 )
+.mult( shape(100,() => a.fft[0] + 0.5,.01).scale(1,.6,1) )
 .out()
+
+s0.initScreen()
+src(s0).scrollX(0.5, a.fft[3]-0.5).scrollY(0.2, a.fft[2]-0.5).out(o1)
