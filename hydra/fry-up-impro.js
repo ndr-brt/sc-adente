e0a195075e3dde64f5f11e377a0adca22a437c30
ndr_brt <andrea.bertagnolli@gmail.com>
2024-09-23 17:10:57 +0200

    gho perso meza roba diogato

--- hydra/fry-up-impro.js
+++ hydra/fry-up-impro.js
@@ -1,5 +1,5 @@
 a.show()
-a.setSmooth(0.2)
+a.setSmooth(0.1)
 a.setBins(4);
 
 // noise(() => 6 * a.fft[1],.05)
@@ -7,21 +7,23 @@ src(o1)
 .mult( osc(9,0, ()=>Math.sin(time/1.5)+2 ) )
 .mult(
     noise(9,.03).brightness(3.2).contrast(2)
-    .mult( osc(9,0, ()=>Math.sin(time/3)+13 ) )
+    .mult(osc(9,0, ()=>Math.sin(time/3)+13 ) )
 )
-// .diff(
-//     noise(15,.04).brightness(.2).contrast(1.3)
-//     .mult( osc(9,0, ()=>Math.sin(time/5)+13 ) )
-//     .rotate( ()=>time )
-// )
-// .scale( ()=>Math.sin(time/6.2)*.12+.15 )
-// .modulateScale(
-//     osc(16,() => a.fft[2],0).mult( osc(3,0,0).rotate(3.14/2) )
-//     .rotate( ()=>time/25 ).scale(.39).scale(1,.6,1).invert()
-//     , ()=>Math.sin(time/5.3)*1.5+3  )
-// .rotate( ()=>time/22 )
-.mult( shape(100,() => a.fft[0] + 0.5,.01).scale(1,.6,1) )
+.diff(
+    noise(15,.04).brightness(.2).contrast(1.3)
+    .mult( osc(9,0, ()=>Math.sin(time/5)+13 ) )
+    .rotate( ()=>time )
+)
+.scale( ()=>Math.sin(time/6.2)*.12+.15 )
+.modulateScale(
+    osc(16,() => a.fft[2],0).mult( osc(3,0,0).rotate(3.14/2) )
+    .rotate( ()=>time/25 ).scale(.39).scale(1,.6,1).invert()
+    , ()=>Math.sin(time/5.3)*1.5+3  )
+.rotate( ()=>time/22 )
+.mult(shape(100,() => a.fft[1] + 0.2,.01).scale(1,.6,1)
+  .scrollX(a.fft[2]+0.9).scrollY(a.fft[3]+0.7)
+)
 .out()
 
 s0.initScreen()
-src(s0).scrollX(0.5, a.fft[3]-0.5).scrollY(0.2, a.fft[2]-0.5).out(o1)
+src(s0).out(o1)
